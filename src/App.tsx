b721c08983f90deb94415ef580ec66d82c81dd18
Anas Attoum <anasattoum.12321@gmail.com>
2024-11-21 08:53:39 +0300

    Lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { lazy, Suspense } from "react";
 
 import Header from "./components/Header";
-import Home from "./pages/Home";
+import Loading from "./pages/Loading";
+
+const LazyHome = lazy(() => import("./pages/Home"));
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Header />}>
-          <Route index element={<Home />} />
+          <Route index element={<Suspense fallback={<Loading />}><LazyHome /></Suspense>} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/pages/Loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+        <div className="flex justify-center items-center" style={{minHeight:'calc(100vh - 70px)'}}>
+            <div className="loading"></div>
+        </div>
+  );
+}
